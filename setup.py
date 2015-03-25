b3882288da947c2ed0cc0122963311cbb8da9a15
Marc Brinkmann <git@marcbrinkmann.de>
2015-03-25 11:33:31 +0100

    Initial import.

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='latex',
+    version='0.1dev',
+    description='Wrappers for calling LaTeX/building LaTeX documents.',
+    long_description=read('README.rst'),
+    author='Marc Brinkmann',
+    author_email='git@marcbrinkmann.de',
+    url='http://github.com/mbr/latex',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[],
+)
