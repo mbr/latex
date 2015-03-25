780f886ff0d810cf68d8faa9db3d03ec1a945129
Marc Brinkmann <git@marcbrinkmann.de>
2015-03-25 11:34:40 +0100

    Added jinja2 contrib.

---
+++ latex/jinja2.py
@@ -0,0 +1,49 @@
+from markupsafe import Markup
+from tex import escape_latex
+from jinja2 import Environment
+
+
+class LatexMarkup(Markup):
+    def unescape(self):
+        raise NotImplementedError
+
+    def stripstags(self):
+        raise NotImplementedError
+
+    @classmethod
+    def escape(cls, s):
+        if hasattr(s, '__html__'):
+            return s.__html__()
+
+        rv = escape_latex(s)
+        if rv.__class__ is not cls:
+            return cls(rv)
+        return rv
+
+
+escape = LatexMarkup.escape
+
+
+ENV_ARGS = {
+    'block_start_string': '\BLOCK{',
+    'block_end_string': '}',
+    'variable_start_string': '\VAR{',
+    'variable_end_string': '}',
+    'comment_start_string': '\#{',
+    'comment_end_string': '}',
+    'line_statement_prefix': '%-',
+    'line_comment_prefix': '%#',
+    'trim_blocks': True,
+    'autoescape': False,
+}
+
+
+def make_env(*args, **kwargs):
+    ka = ENV_ARGS.copy()
+    ka.update(kwargs)
+
+    env = Environment(*args, **ka)
+    env.filters['e'] = LatexMarkup.escape
+    env.filters['escape'] = LatexMarkup.escape
+    env.filters['forceescape'] = LatexMarkup.escape  # FIXME: this is a bug
+    return env
