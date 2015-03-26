b7c527dccf4bbef2b1e42c27bb322c140b13aacb
Marc Brinkmann <git@marcbrinkmann.de>
2015-03-26 14:04:15 +0100

    Release version 0.3.
    (commit by unleash 0.5.8.dev1)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='latex',
-    version='0.1dev',
+    version='0.3',
     description='Wrappers for calling LaTeX/building LaTeX documents.',
     long_description=read('README.rst'),
     author='Marc Brinkmann',
