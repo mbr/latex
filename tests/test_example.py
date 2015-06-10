429c4a93ee245b34aaf2b4a60e7e0ff7403fa91a
Marc Brinkmann <git@marcbrinkmann.de>
2015-06-10 15:44:39 +0200

    Added example to test cases.

---
+++ tests/test_example.py
@@ -0,0 +1,13 @@
+def test_run_example():
+    min_latex = (r"\documentclass{article}"
+                 r"\begin{document}"
+                 r"Hello, world!"
+                 r"\end{document}")
+
+    from latex import build_pdf
+
+    # this builds a pdf-file inside a temporary directory
+    pdf = build_pdf(min_latex)
+
+    # look at the first few bytes of the header
+    print (str(pdf)[:10])
