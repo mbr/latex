ab84c37195feb7ea19be810a7d1a899e5e53ee78
Marc Brinkmann <git@marcbrinkmann.de>
2015-06-12 11:47:34 +0200

    Test whether or not the right exception is thrown.

--- tests/test_pdfbuild.py
+++ tests/test_pdfbuild.py
@@ -1,4 +1,7 @@
 from latex import build_pdf
+from latex.exc import LatexBuildError
+
+import pytest
 
 
 def test_generates_something():
@@ -12,3 +15,10 @@ Hello, world!
     pdf = build_pdf(min_latex)
 
     assert pdf
+
+
+def test_raises_correct_exception_on_fail():
+    broken_latex = r"""foo"""
+
+    with pytest.raises(LatexBuildError):
+        build_pdf(broken_latex)
