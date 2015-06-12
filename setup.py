648189c3a14316b50eb0e90d42577d5f4ccd31e1
Marc Brinkmann <git@marcbrinkmann.de>
2015-06-12 12:19:57 +0200

    Start developing version 0.5.6.dev1 (after release of 0.5.5)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='latex',
-    version='0.5.5.dev1',
+    version='0.5.6.dev1',
     description='Wrappers for calling LaTeX/building LaTeX documents.',
     long_description=read('README.rst'),
     author='Marc Brinkmann',
