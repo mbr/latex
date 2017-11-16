eb06e85c7dcb93febe22d20cd7e3e694939449ba
Marc Brinkmann <git@marcbrinkmann.de>
2017-11-16 11:45:15 +0100

    Make XeLaTeX test harder (to actually test xelatex being used).

--- tests/test_xelatex.py
+++ tests/test_xelatex.py
@@ -2,10 +2,25 @@ from latex.build import LatexMkBuilder
 
 
 def test_xelatex():
+    # the example below should not compile on pdflatex, but on xelatex
     min_latex = r"""
-\documentclass{article}
+\documentclass[12pt]{article}
+\usepackage{fontspec}
+
+\setmainfont{Times New Roman}
+
+ \title{Sample font document}
+ \author{Hubert Farnsworth}
+ \date{this month, 2014}
+
 \begin{document}
-Hello, world!
+
+ \maketitle
+
+ This an \textit{example} of document compiled
+ with \textbf{xelatex} compiler. LuaLaTeX should
+ work fine also.
+
 \end{document}
 """
     builder = LatexMkBuilder(variant='xelatex')
