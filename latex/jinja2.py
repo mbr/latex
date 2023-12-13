425b37d147c69aa5d949dbd48b9f9e42d5e22b2d
John te Bokkel <john@powercoreeng.com>
2023-12-13 15:39:46 -0800

    fix: SyntaxWarning for invalid escape sequence

--- latex/jinja2.py
+++ latex/jinja2.py
@@ -25,11 +25,11 @@ class LatexMarkup(Markup):
 
 
 ENV_ARGS = {
-    'block_start_string': '\BLOCK{',
+    'block_start_string': '\\BLOCK{',
     'block_end_string': '}',
-    'variable_start_string': '\VAR{',
+    'variable_start_string': '\\VAR{',
     'variable_end_string': '}',
-    'comment_start_string': '\#{',
+    'comment_start_string': '\\#{',
     'comment_end_string': '}',
     'line_statement_prefix': '%-',
     'line_comment_prefix': '%#',
